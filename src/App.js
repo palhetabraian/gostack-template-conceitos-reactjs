ae7e2e11ebc66ccef8caa9618bb0d12afeb0c9e3
Braian <134152507+palhetabraian@users.noreply.github.com>
2025-12-28 14:50:43 -0300

    Finalizando exercicio

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
-import "./styles.css";
+import './styles.css';
 
 function App() {
-  async function handleAddRepository() {
     // TODO
-  }
+    //Estate dos repositorios
+    const [repositories, setRepositories] = useState([]);
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+    useEffect(() => {
+        // buscando os dados com get
+        api.get('/repositories').then((response) => {
+            setRepositories(response.data);
+        });
+    }, []);
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    async function handleAddRepository() {
+        const response = await api.post('repositories', {
+            title: 'Umbriel',
+            url: 'https://github.com/rocketseat/umbriel',
+            techs: ['node', 'react'],
+        });
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+        setRepositories([...repositories, response.data]);
+    }
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+    async function handleRemoveRepository(id) {
+        // TODO
+        await api.delete(`repositories/${id}`);
+
+        setRepositories(repositories.filter((repository) => repository.id !== id));
+    }
+
+    return (
+        <div>
+            <ul data-testid="repository-list">
+                {repositories.map((repository) => (
+                    <li key={repository.id}>
+                        {repository.title}
+                        <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+                    </li>
+                ))}
+            </ul>
+
+            <button onClick={handleAddRepository}>Adicionar</button>
+        </div>
+    );
 }
 
 export default App;
